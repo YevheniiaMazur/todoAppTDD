afdeab4c7fe2616e739222a33a47a48c3d726e0e
Yevheniia Mazur <yevheniia.mazur@valor-software.com>
2017-10-03 18:11:41 +0300

    add sauce

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,20 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
+  if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
+    console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
+    process.exit(1)
+  }
+
+  var customLaunchers = {
+    sl_chrome: {
+      base: 'SauceLabs',
+      browserName: 'chrome',
+      platform: 'Linux',
+      version: '48'
+    }
+  };
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular/cli'],
@@ -10,7 +24,8 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
+      require('@angular/cli/plugins/karma'),
+      require('karma-sauce-launcher')
     ],
     client:{
       clearContext: false // leave Jasmine Spec Runner output visible in browser
@@ -31,14 +46,27 @@ module.exports = function (config) {
     angularCli: {
       environment: 'dev'
     },
-    reporters: config.angularCli && config.angularCli.codeCoverage
-              ? ['progress', 'coverage-istanbul']
-              : ['progress', 'kjhtml'],
+    // reporters: config.angularCli && config.angularCli.codeCoverage
+    //           ? ['progress', 'coverage-istanbul']
+    //           : ['progress', 'kjhtml'],
+
+    reporters: ['progress', 'saucelabs'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    sauceLabs: {
+      testName: 'todo tests',
+      recordScreenshots: false,
+      connectOptions: {
+        port: 5757,
+        logfile: 'sauce_connect.log'
+      }
+    },
+    // logLevel: config.LOG_INFO,
+    // autoWatch: true,
+    // browsers: ['Chrome'],
+    captureTimeout: 120000,
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    singleRun: true
   });
 };
